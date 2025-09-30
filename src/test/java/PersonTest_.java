4a790a8ff4d773e7d6194349653787a3f598cbcb
Felipe Vieira <santosvieira.felipe@gmail.com>
2025-09-30 20:06:36 -0300

    0. Password

---
+++ src/test/java/PersonTest_.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTest_ {
+
+    private Person_ person;
+
+    @BeforeEach
+    public void setup() {
+        person = new Person_("Paul", "McCartney", new Date(2000, 1, 1), true, true, true);
+    }
+
+    @Test
+    public void show_full_name() {
+        assertEquals("Paul McCartney", person.fullName());
+    }
+
+    @Test
+    public void test_calculateYearlySalary() {
+        person.setSalary(1200);
+        assertEquals(14400, person.calculateYearlySalary());
+    }
+
+    @Test
+    public void person_is_MEI() {
+        person.setAnotherCompanyOwner(false);
+        person.setPensioner(false);
+        person.setPublicServer(false);
+        person.setSalary(1200);
+        assertTrue(person.isMEI());
+    }
+
+    @Test
+    public void person_is_not_MEI() {
+        assertFalse(person.isMEI());
+    }
+}
